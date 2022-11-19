8e0cae6deb2975553a60a22d44d261c0dd31ad22
waisa4613 <59243911+waisa4613@users.noreply.github.com>
2022-11-19 16:41:03 +0900

    commit

--- Assets/Scripts/MoveObject.lua
+++ Assets/Scripts/MoveObject.lua
@@ -2,29 +2,102 @@ require "AdHoc"
 
 local this = GetThis()
 local entities = {}
+local entitiesHitFlg = {}
+local entitiesObjectFlag = {}
 local nextId = 0
+local once =false
+
+local fbxname={}
+fbxname[1]="bed.obj"
+fbxname[2]="dai.obj"
+fbxname[3]="sofa_double.obj"
+fbxname[4]="table.obj"
+
+local reel = {}
+
+
+local ObjectType = [[
+    value = 10
+    Type = 10
+    function GetValue(x)
+        return value * x
+    end
+
+    function Start()
+        
+    end
+
+    function TypeObject(_x)
+        Type=_x
+    end
+
+    
+]]
 
 function Start()
     entities[nextId] = CreateEntity()
     local transforms = GetComponent(entities[nextId], "Transform")
-    transforms.scale.x = 0.1
-    transforms.scale.y = 0.1
-    transforms.scale.z = 0.1
-    --local meshes = GetComponent(entities[nextId], "Mesh")
-    --meshes:Load("cube.obj")
+    -- if reel[nextId]==0 then
+    --     transforms.scale.x = 0.1
+    --     transforms.scale.y = 0.1
+    --     transforms.scale.z = 0.1
+    -- elseif reel[nextId]==1 then
+    --     transforms.scale.x = 0.075
+    --     transforms.scale.y = 0.075
+    --     transforms.scale.z = 0.075
+    -- else
+    --     transforms.scale.x = 0.05
+    --     transforms.scale.y = 0.05
+    --     transforms.scale.z = 0.05
+    -- end
+    local Ramdom=math.random(0,2)
+    if Ramdom==0 then
+        transforms.scale.x = 0.1
+        transforms.scale.y = 0.1
+        transforms.scale.z = 0.1
+    elseif Ramdom==1 then
+        transforms.scale.x = 0.075
+        transforms.scale.y = 0.075
+        transforms.scale.z = 0.075
+    else
+        transforms.scale.x = 0.05
+        transforms.scale.y = 0.05
+        transforms.scale.z = 0.05
+    end
+    
     AddComponent(entities[nextId], "RigidBody", "Box", "Dynamic")
+    AddComponent(entities[nextId],"Script",ObjectType)
     local tempRigidbody = GetComponent(entities[nextId], "RigidBody")
     tempRigidbody:SetTranslation(0.0 , 0.5, -1.0 )
     
     tempRigidbody:SetVelocity(0,0,0.2)
     tempRigidbody:SetHasGravity(false)
     tempRigidbody:SetTrigger(true)
-    --tempRigidbody:SetRotation(math.rad(-90),0,0)
     tempRigidbody:UpdateGeometry()
+    
+    -- local s = GetComponent(entities[0], "Script")
+    -- LogMessage(s:Get("value"))
+    -- LogMessage(s:Call("GetValue", 2))
     nextId=nextId+1
+   
 end
 
 function Update()
+    if once==false then
+        for i = 0, nextId-1 do
+            local m=math.random(1,4)
+            local s = GetComponent(entities[i], "Script")
+            LogMessage(s:Call("TypeObject", m))
+            LogMessage(s:Get("Type"))
+            
+            -- local m=fbxname[reel[i]]
+            -- local Ramdom=math.random(1,5)
+            -- local m=fbxname[Ramdom]
+            
+           
+        end
+        once=true
+    end
     --位置のループ処理
     local transforms = GetComponent(entities[0], "Transform")
     local tempRigidbody = GetComponent(entities[0], "RigidBody")
@@ -32,6 +105,9 @@ function Update()
         tempRigidbody:SetTranslation(0.0 , 0.5, -1.0 )
         tempRigidbody:UpdateGeometry()
     end
+   
+
+   
 
     --範囲ないなら緑に光る
     if transforms.translate.z>-0.1 and transforms.translate.z<0.1 then 
---
+++ Assets/Scripts/RayHitPresent.lua
@@ -0,0 +1,82 @@
+require "AdHoc"
+
+local this = GetThis()
+local input = GetInput()
+local t2={}
+t2["RayFront"]=Vector3D:new()
+
+local fbxname={}
+fbxname[1]="bed.obj"
+fbxname[2]="dai.obj"
+fbxname[3]="sofa_double.obj"
+fbxname[4]="table.obj"
+
+function MoveCrossHair()
+    local transform = GetComponent(this, "Transform")
+    local Speed=0.02
+    
+    if input:GetKey(AdHoc.Key.w) then
+        transform.translate.z = transform.translate.z + Speed
+       
+    elseif input:GetKey(AdHoc.Key.s) then
+        transform.translate.z = transform.translate.z - Speed
+      
+    elseif input:GetKey(AdHoc.Key.a) then
+        transform.translate.x = transform.translate.x - Speed
+       
+    elseif input:GetKey(AdHoc.Key.d) then
+        transform.translate.x = transform.translate.x + Speed
+       
+    end
+    local transforms = GetComponent(this, "Transform")
+    transforms.translate.x = transform.translate.x
+    transforms.translate.y = 1
+    transforms.translate.z = transform.translate.z
+end
+
+function RayHit()
+    local transform = GetComponent(this, "Transform")
+    local e = Raycast(transform.translate, t2["RayFront"], 0.5)
+    if e ~= 0 then
+        local m = GetComponent(this, "Material")
+        m.albedo.x =1
+        m.albedo.y = 0
+        m.albedo.z = 0
+        if input:GetKey(AdHoc.Key.space) then
+            material=GetComponent(e,"Material")
+            material.albedo.x=0
+            material.albedo.y=0
+            material.albedo.z=1
+            local transforms = GetComponent(e, "Transform")
+            transforms.scale.x = 0.05
+            transforms.scale.y = 0.05
+            transforms.scale.z = 0.05
+            local s = GetComponent(e, "Script")
+            local ObjectType=s:Get("Type")
+            local meshes = GetComponent(e, "Mesh")
+            meshes:Load(fbxname[ObjectType])
+
+            
+            local tempRigidbody = GetComponent(e, "RigidBody")
+            tempRigidbody:SetVelocity(0,0,0)
+            tempRigidbody:SetTranslation(transforms.translate.x, 0.1, transforms.translate.z )
+            tempRigidbody:UpdateGeometry()
+       end
+    else
+        local m = GetComponent(this, "Material")
+        m.albedo.x = 1
+        m.albedo.y =1
+        m.albedo.z =1
+    end
+end
+
+function Start()
+    t2["RayFront"].x=0
+    t2["RayFront"].y=-1
+    t2["RayFront"].z=0
+end
+
+function Update()
+    MoveCrossHair()
+    RayHit()
+end
